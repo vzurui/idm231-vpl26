bb1df96cd65235107ddfc9eaad4de46f7656e7de
Vy Le <139026534+vzurui@users.noreply.github.com>
2024-02-07 21:14:06 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-const button = document.getElementById("submit-button");
-// 1. get our button, add listener, click, IT will call the select button function by itself
-button.addEventListener("click", select_button);
+// const button = document.getElementById("submit-button");
+// // 1. get our button, add listener, click, IT will call the select button function by itself
+// button.addEventListener("click", select_button);
 // DO NOT for event listeners call the function early. If you do,
 // it will do it's thing and walk away.
 // You want the click ACTION to CALL the function
@@ -88,46 +88,90 @@ function get_month() {
   return ampharos_button;
 }*/
 
-// function select_button() {
-//   const birthday 
-//   const birthday_month
-//   const birthday_day
-// }
+const birthday = document.getElementById("birthday-pick");
 
-// const first_zodiac = {
-//   name: 'Mr. Frond',
-//   Image: 'mr_frond.jpg'
-// }
+const mr_frond_first_zodiac = {
+  name: "Mr. Frond",
+  image: "mr_frond.jpg",
+};
 
-// function first_zodiac() {
+const tammy_second_zodiac = {
+  name: "Tammy",
+  image: "tammy.jpg",
+};
 
-// }
+const teddy_third_zodiac = {
+  name: "Teddy",
+  image: "teddy.jpg",
+};
 
-function 
+const louise_fourth_zodiac = {
+  name: "Louise",
+  image: "louise.jpg",
+};
+
+const jimmy_fifth_zodiac = {
+  name: "Jimmy Jr",
+  image: "jimmy_junior.jpg",
+};
+
+const linda_sixth_zodiac = {
+  name: "Linda",
+  image: "linda.jpg",
+};
+
+const bob_seventh_zodiac = {
+  name: "Bob",
+  image: "bob.jpg",
+};
+
+const kk_eigth_zodiac = {
+  name: "Kuchi Kopi",
+  image: "kuchi_kopi.jpg",
+};
+
+const rudy_ninth_zodiac = {
+  name: "Rudy",
+  image: "rudy.jpg",
+};
+
+const tina_tenth_zodiac = {
+  name: "Tina",
+  image: "Tina_Belcher.jpg",
+};
+
+const gayle_eleventh_zodiac = {
+  name: "Gayle",
+  image: "gayle.jpg",
+};
+
+const gene_twelvth_zodiac = {
+  name: "Gene",
+  image: "gene.jpg",
+};
 
 if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-  astrological_sign = 'Capricorn'
+  astrological_sign = "Capricorn";
 } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-  astrological_sign = 'Sagittarius'
+  astrological_sign = "Sagittarius";
 } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-  astrological_sign = 'Scorpio'
+  astrological_sign = "Scorpio";
 } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-  astrological_sign = 'Libra'
+  astrological_sign = "Libra";
 } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-  astrological_sign = 'Virgo'
+  astrological_sign = "Virgo";
 } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-  astrological_sign = 'Leo'
+  astrological_sign = "Leo";
 } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-  astrological_sign = 'Cancer'
+  astrological_sign = "Cancer";
 } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-  astrological_sign = 'Gemini'
+  astrological_sign = "Gemini";
 } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-  astrological_sign = 'Taurus'
+  astrological_sign = "Taurus";
 } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-  astrological_sign = 'Aries'
+  astrological_sign = "Aries";
 } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-  astrological_sign = 'Pisces'
+  astrological_sign = "Pisces";
 } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-  astrological_sign = 'Aquarius'
+  astrological_sign = "Aquarius";
 }
-
