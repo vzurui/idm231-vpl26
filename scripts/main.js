91b6c8ee71ad830cd99cc278b16c0e1bc76b6e54
Vy Le <139026534+vzurui@users.noreply.github.com>
2024-02-16 00:19:23 -0500

    updates
    playin around...

--- scripts/main.js
+++ scripts/main.js
@@ -1,94 +1,9 @@
-// const button = document.getElementById("submit-button");
-// // 1. get our button, add listener, click, IT will call the select button function by itself
-// button.addEventListener("click", select_button);
-// DO NOT for event listeners call the function early. If you do,
-// it will do it's thing and walk away.
-// You want the click ACTION to CALL the function
-// DON'T DO THIS: button.addEventListener('click', select_button())
-
-/*
-This is an example of an anonymous function which is
-THE SAME as select_button
-
-button.addEventListener('click', () => {
-    const month = get_month()
-    const ampharos_button_copy = get_ampharos_button().cloneNode(true)
-    // const ampharos_button_copy = document.querySelector('.ampharos').cloneNode(true)
-    const mew_button_copy = document.querySelector('.mew').cloneNode(true)
-    const display_pokemon_container = document.querySelector(".display-pokemon")
-    const firstChild = display_pokemon_container.firstChild
-
-    function get_month() {
-        const month = document.getElementById('birthday-options').value
-        return month
-    }
-    // remove the first child of display pokemon container
-    if (firstChild) {
-        display_pokemon_container.removeChild(firstChild)
-    }
-
-    if (month == 'January') {
-        display_pokemon_container.appendChild(ampharos_button_copy)
-    }
-
-    function get_month() {
-        const month = document.getElementById('birthday-options').value
-        return month
-    }
-
-    function get_ampharos_button() {
-        const ampharos_button = document.querySelector('.ampharos')
-        return ampharos_button
-    }
-})
-*/
-
-// 2. when it is called, it will console log get_select_field_value
-/*
-function select_button() {
-  const month = get_month();
-  const mr_frond_button_copy = document
-    .querySelector(".mr-frond")
-    .cloneNode(true);
-  const tammy_button_copy = document.querySelector(".tammy").cloneNode(true);
-  const teddy_button_copy = document.querySelector(".teddy").cloneNode(true);
-  const louise_button_copy = document.querySelector(".louise").cloneNode(true);
-  // const ampharos_button_copy = document.querySelector('.ampharos').cloneNode(true)
-  //   const mew_button_copy = document.querySelector(".mew").cloneNode(true);
-  const display_bb_container = document.querySelector(".display-bb");
-  const firstChild = display_bb_container.firstChild;
-
-  // remove the first child of display pokemon container
-  if (firstChild) {
-    display_bb_container.removeChild(firstChild);
-  }
-
-  if (month == "January") {
-    display_bb_container.appendChild(mr_frond_button_copy);
-  } else if (month == "February") {
-    display_bb_container.appendChild(tammy_button_copy);
-  } else if (month == "March") {
-    display_bb_container.appendChild(teddy_button_copy);
-  } else if (month == "April") {
-    append_bb_button(louise_button_copy);
-  }
-
-  function append_bb_button(button) {
-    return display_bb_container.appendChild(button);
-  }
-}
-
-function get_month() {
-  const month = document.getElementById("birthday-options").value;
+function select_bday() {
+  const birthday = document.getElementById("birthday-pick").value;
   return month;
 }
 
- function get_ampharos_button() {
-  const ampharos_button = document.querySelector(".ampharos");
-  return ampharos_button;
-}*/
-
-const birthday = document.getElementById("birthday-pick");
+document.querySelector(".mr-frond").getd;
 
 const mr_frond_first_zodiac = {
   name: "Mr. Frond",
