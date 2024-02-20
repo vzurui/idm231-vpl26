51183182b3be63065b8d2d04369c8c52bd60de36
Vy Le <139026534+vzurui@users.noreply.github.com>
2024-02-19 23:48:55 -0500

    js update

--- scripts/main.js
+++ scripts/main.js
@@ -70,27 +70,27 @@ function close_zodiac() {
 }
 
 if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
-  astrological_sign = "Capricorn";
+  astrological_sign = "mr-frond";
 } else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
-  astrological_sign = "Sagittarius";
+  astrological_sign = "gene";
 } else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
-  astrological_sign = "Scorpio";
+  astrological_sign = "kuchi-kopi";
 } else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
-  astrological_sign = "Libra";
+  astrological_sign = "tina";
 } else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
-  astrological_sign = "Virgo";
+  astrological_sign = "jimmy-jr";
 } else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
-  astrological_sign = "Leo";
+  astrological_sign = "tammy";
 } else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
-  astrological_sign = "Cancer";
+  astrological_sign = "rudy";
 } else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
-  astrological_sign = "Gemini";
+  astrological_sign = "linda";
 } else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
-  astrological_sign = "Taurus";
+  astrological_sign = "bob";
 } else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
-  astrological_sign = "Aries";
+  astrological_sign = "louise";
 } else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
-  astrological_sign = "Pisces";
+  astrological_sign = "teddy";
 } else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
-  astrological_sign = "Aquarius";
+  astrological_sign = "gayle";
 }
